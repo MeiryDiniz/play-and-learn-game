51dbae2a5812476b7b41c7cba62908c871da48ab
MeiryDiniz <>
2023-09-08 00:00:09 +0000

    Add style to the gamearea.html and function to go back to the home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,10 @@
 
 document.getElementById('start-button').addEventListener('click', play);
 function play () {window.location.href = 'gamearea.html';}
+
+/**
+ * Function to go back to the home page
+ */
+
+document.getElementById('home-page').addEventListener('click', home);
+function home () {window.location.href = '../index.html';}
